393051474d994e1c2413708780c12eac1b6b3ba7
SakshamShrestha12 <163832784+SakshamShrestha12@users.noreply.github.com>
2025-05-25 21:43:52 +0545

    Reupdated deployment try

--- routes/qr.js
+++ routes/qr.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const QrCode = require("../models/QRCode");
+const QrCode = require("../models/QrCode");
 const router = express.Router();
 
 // Multer setup
